8f94d148c526e63a72cf029fd563044878742f1f
MayaGavrichenkova <apelsinkot2003@gmail.com>
2025-07-15 23:48:44 +0300

    Обновлены Synchro и README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Basler_cam
+https://github.com/basler/pypylon/tree/master - гит с хорошим кодом для basler
+
+Для корректной работы кода синхронизации на macOS необходимо:
+Указать тип камеры, в случае работы с acA 1600 60 gm- GigEcamera
+Ограничить размер посыдаемых пакетов 
+Прописать задержку между пакетами от двух камер
+
+
+На macOS от Basler совместим только Basler Socket Driver- не настоящий драйвер, а способ взаимодействия через стандартный сокет
+Ссылка на источник: https://assets-ctf.baslerweb.com/dg51pdwahxgw/2cevzPRHqqWzf1tiQh6rgn/e562d5084d6fd699c077ec45c27497d3/AW00144501000_GigE_Vision_Network_Drivers_and_Bandwidth_Management.pdf?utm_source=chatgpt.com 
--- synchro.py
+++ synchro.py
@@ -2,79 +2,97 @@ from pypylon import pylon
 import cv2
 from pypylon import genicam
 import time
-tl_factory = pylon.TlFactory.GetInstance()
-devices = tl_factory.EnumerateDevices()
-master = pylon.InstantCamera(tl_factory.CreateDevice(devices[0]))
-slave = pylon.InstantCamera(tl_factory.CreateDevice(devices[1]))
 
-master.Open()
-slave.Open()
-# print(master.LineMode.GetSymbolics())
-# for line in master.LineSelector.GetSymbolics():
-#     master.LineSelector.SetValue(line)
-#     modes = master.LineMode.GetSymbolics()
-#     print(f"{line}: {modes}")
+
+path="/Users/majagavricenkova/Desktop/frames"#прописать свой путь
+tl_factory=pylon.TlFactory.GetInstance()
+devices=tl_factory.EnumerateDevices()
+
+cameras=pylon.InstantCameraArray(2)#захват нескольких камер
+for i,cam in enumerate(cameras):
+    cam.Attach(tl_factory.CreateDevice(devices[i]))#привязывает объект кам к реальной физической камере из списка девайсов
+    cam.SetCameraContext(i)#к каждой камере номер
+    cam.Open()
+    if cam.GetDeviceInfo().GetDeviceClass()=='BaslerGigE':
+        if cam.GevSCPSPacketSize.IsWritable():
+            cam.GevSCPSPacketSize.SetValue(1440)#размер udp пакета
+        if cam.GevSCPD.IsWritable():
+            cam.GevSCPD.SetValue(10000)#задержка между отправкой
+master=cameras[0]
 master.TriggerSelector.SetValue("AcquisitionStart")
 master.TriggerMode.SetValue("On")
 master.TriggerSource.SetValue("Software")
 master.TriggerActivation.SetValue("RisingEdge")
 
-
-
-master.LineSelector.SetValue("Out1")
-master.LineMode.SetValue("Output")
-master.LineSource.SetValue("ExposureActive")
-master.LineFormat.SetValue('OptoCoupled')
-
-slave.LineSelector.SetValue("Line1")
-slave.LineMode.SetValue("Input")
-
+slave=cameras[1]
 slave.TriggerSelector.SetValue("AcquisitionStart")
 slave.TriggerMode.SetValue("On")
+slave.LineSelector.SetValue("Line1")
+slave.LineMode.SetValue("Input")
 slave.TriggerSource.SetValue("Line1")
 slave.TriggerActivation.SetValue("RisingEdge")
 slave.TriggerDelayAbs.SetValue(0.0002)
 
-slave.StartGrabbing()
-master.StartGrabbing()
-img_master = pylon.PylonImage()
-img_slave=pylon.PylonImage()
-path='/Users/maya/Desktop/frames/'
-
-#master.ExecuteSoftwareTrigger()
-
-# converter = pylon.ImageFormatConverter()
-# converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-# converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
-
+cameras.StartGrabbing()
 try:
-    while master.IsGrabbing() and slave.IsGrabbing():
-        #master.AcquisitionStart.Execute()
+    for i in range(100):
         master.ExecuteSoftwareTrigger()
-        master_gr = master.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-        slave_gr = slave.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-
-        if master_gr.GrabSucceeded() and slave_gr.GrabSucceeded():
-            filename_master="pylon_img_master_save" + str(time.time()) + ".tiff" 
-            filename_slave="pylon_img_slave_save" + str(time.time()) + ".tiff" 
-            # master_img = converter.Convert(master_gr).GetArray()
-            # slave_img = converter.Convert(slave_gr).GetArray()
-            img_master.AttachGrabResultBuffer(master_gr)
-            img_slave.AttachGrabResultBuffer(slave_gr)
-            img_master.Save(pylon.ImageFileFormat_Tiff, path+filename_master)
-            img_slave.Save(pylon.ImageFileFormat_Tiff, path+filename_slave)
-            # cv2.imshow("Master", img_master)
-            # cv2.imshow("Slave", img_slave)
-
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
-
-        master_gr.Release()
-        slave_gr.Release()
-
+        for _ in range(2):
+            res=cameras.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+            context=res.GetCameraContext()#каждый раз узнаем от кого пришел кадр
+            img=pylon.PylonImage()
+            img.AttachGrabResultBuffer(res)
+            filename="pylon_img_save" + str(time.time()) + str(context)+".tiff" 
+            img.Save(pylon.ImageFileFormat_Tiff, path+filename)
+            res.Release()
 finally:
-    master.StopGrabbing()
-    slave.StopGrabbing()
-    master.Close()
-    slave.Close()
-    cv2.destroyAllWindows()
+    cam.StopGrabbing()
+    for cam in cameras:
+        cam.Close()
+# print(master.LineMode.GetSymbolics())
+# for line in master.LineSelector.GetSymbolics():
+#     master.LineSelector.SetValue(line)
+#     modes = master.LineMode.GetSymbolics()
+#     print(f"{line}: {modes}")
+# slave.StartGrabbing()
+# # img_master = pylon.PylonImage()
+# # img_slave=pylon.PylonImage()
+# # path='/Users/maya/Desktop/frames/'
+
+# #master.ExecuteSoftwareTrigger()
+
+# # converter = pylon.ImageFormatConverter()
+# # converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+# # converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+# try:
+#     while master.IsGrabbing() and slave.IsGrabbing():
+#         #master.AcquisitionStart.Execute()
+#         master.ExecuteSoftwareTrigger()
+#         master_gr = master.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+#         slave_gr = slave.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+#         if master_gr.GrabSucceeded() and slave_gr.GrabSucceeded():
+#             filename_master="pylon_img_master_save" + str(time.time()) + ".tiff" 
+#             filename_slave="pylon_img_slave_save" + str(time.time()) + ".tiff" 
+#             # master_img = converter.Convert(master_gr).GetArray()
+#             # slave_img = converter.Convert(slave_gr).GetArray()
+#             img_master.AttachGrabResultBuffer(master_gr)
+#             img_slave.AttachGrabResultBuffer(slave_gr)
+#             img_master.Save(pylon.ImageFileFormat_Tiff, path+filename_master)
+#             img_slave.Save(pylon.ImageFileFormat_Tiff, path+filename_slave)
+#             # cv2.imshow("Master", img_master)
+#             # cv2.imshow("Slave", img_slave)
+
+#             # if cv2.waitKey(1) & 0xFF == ord('q'):
+#             #     break
+
+#         master_gr.Release()
+#         slave_gr.Release()
+
+# finally:
+#     master.StopGrabbing()
+#     slave.StopGrabbing()
+#     master.Close()
+#     slave.Close()
+#     cv2.destroyAllWindows()
